2ed8db8e25a36837f0b951b9bdaf249d5bd575bc
Andy Smith <iamasmith.home@gmail.com>
2023-05-13 14:24:48 +0100

    Add logger setup

---
+++ internal/logging/logging.go
@@ -0,0 +1,14 @@
+package logging
+
+import (
+	"github.com/iamasmith/logtozap"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func Setup() *zap.SugaredLogger {
+	z := zap.NewProductionConfig()
+	logger := zap.Must(z.Build()).Sugar()
+	logtozap.ToSugared(logger, zapcore.InfoLevel)
+	return logger
+}
